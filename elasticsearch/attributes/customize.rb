bf9ae655e8790c56d9d531b29d951d46552c7b4b
CA-DavidFoster <david.foster@creative-assembly.com>
2015-09-10 15:45:29 +0100

    Update of customize.rb for our setup.

--- elasticsearch/attributes/customize.rb
+++ elasticsearch/attributes/customize.rb
@@ -1,3 +1,3 @@
-normal[:elasticsearch][:version] = '1.7.0'
-normal[:elasticsearch][:plugins]['elasticsearch/elasticsearch-cloud-aws'][:version] = '2.7.0' # Must correpond to the proper Elasticsearch version. See the elasticsearch-cloud-aws repo for more info.
-normal[:elasticsearch][:cluster][:name] = 'sweet_cluster'  # Change to whatever you want.
+normal[:elasticsearch][:version] = '1.5.0'
+normal[:elasticsearch][:plugins]['elasticsearch/elasticsearch-cloud-aws'][:version] = '2.5.1' # Must correpond to the proper Elasticsearch version. See the elasticsearch-cloud-aws repo for more info.
+normal[:elasticsearch][:cluster][:name] = 'ca_cluster'  # Change to whatever you want.
