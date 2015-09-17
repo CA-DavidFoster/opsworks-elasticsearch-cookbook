52422c61f5631ab3d341471cbae3c61c38f2f3d0
CA-DavidFoster <david.foster@creative-assembly.com>
2015-09-17 11:03:19 +0100

    Update master-node.rb

--- layer-custom/recipes/master-node.rb
+++ layer-custom/recipes/master-node.rb
@@ -1,2 +1,2 @@
-node.default[:elasticsearch][:node][:master] = 'true' # Set this node to act as a master.
-node.default[:elasticsearch][:node][:data] = 'false' # Set this node to not store data.
+node.default[:elasticsearch][:node]['master'] = 'true' # Set this node to act as a master.
+node.default[:elasticsearch][:node]['data'] = 'false' # Set this node to not store data.
