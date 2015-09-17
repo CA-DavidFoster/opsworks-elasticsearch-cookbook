4176fb9259f9fd599f42e149d46ddf1cf63e1baa
CA-DavidFoster <david.foster@creative-assembly.com>
2015-09-17 10:40:39 +0100

    Update customize.rb

--- elasticsearch/attributes/customize.rb
+++ elasticsearch/attributes/customize.rb
@@ -1,5 +1,3 @@
 normal[:elasticsearch][:version] = '1.5.0'
 normal[:elasticsearch][:plugins]['elasticsearch/elasticsearch-cloud-aws'][:version] = '2.5.1' # Must correpond to the proper Elasticsearch version. See the elasticsearch-cloud-aws repo for more info.
 normal[:elasticsearch][:cluster][:name] = 'ca_cluster'  # Change to whatever you want.
-normal[:elasticsearch][:node][:master] = 'false' # Set this node to act as a master.
-normal[:elasticsearch][:node][:data] = 'true' # Set this node to store data.
